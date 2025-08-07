25039141623b1c17e3561d085f3e6c37aee00f40
Akarsh Gupta <akarshg9669@gmail.com>
2025-08-08 03:05:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let DisplayNum = document.getElementById('DisplayNum');
+
+
+function appendValue(val) {
+  DisplayNum.value += val;
+}
+
+function ClearDisplay() {
+  DisplayNum.value = "";
+}
+
+
+function DeleteNum() {
+  DisplayNum.value = DisplayNum.value.slice(0, -1);
+}
+
+
+function calculate() {
+  try {
+    DisplayNum.value = eval(DisplayNum.value);
+  } catch (error) {
+    DisplayNum.value = "Error";
+  }
+}
